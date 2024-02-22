c983234283f470024567f782386fff0a3a831adc
Pedro <pedro.henrique@serrana.sp.gov.br>
2024-02-22 16:53:45 -0300

    First commit after formatting

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")] //POST: api/Account/register
+        public async Task<ActionResult<AppUser>> Register(String username, String password)
+        {
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+        }
+    }
+}
